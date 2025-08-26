931d9abf6dfcdf0cd155618eab4604b14f18e1b0
8083 <8083@tcesc.tc.br>
2025-08-26 12:09:19 -0300

    Projeto inicial

---
+++ src/main/java/com/example/userdataprocessingstorage/UserDataProcessingStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdataprocessingstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataProcessingStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataProcessingStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userdataprocessingstorage/UserDataProcessingStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdataprocessingstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataProcessingStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
